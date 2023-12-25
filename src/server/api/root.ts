7a7211ebfc5e3e37df32ab362202c47cc83fa518
miikaforma <miika.forma@codecontrol.fi>
2023-12-25 22:59:16 +0200

    t3-app template

---
+++ src/server/api/root.ts
@@ -0,0 +1,14 @@
+import { postRouter } from "@/energyapp/server/api/routers/post";
+import { createTRPCRouter } from "@/energyapp/server/api/trpc";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  post: postRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;

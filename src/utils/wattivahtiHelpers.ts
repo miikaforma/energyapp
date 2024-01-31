4d2fb5998811f7897d9e3a0d7b97d7fa554f928e
miikaforma <miika.forma@codecontrol.fi>
2024-01-31 18:31:47 +0200

    Build fixes

--- src/utils/wattivahtiHelpers.ts
+++ src/utils/wattivahtiHelpers.ts
@@ -1,9 +1,9 @@
 // Helper function to format number to Finnish locale with 2 decimal places
-export function formatNumberToFI(num: number | undefined, minDigits: number = 2, maxDigits: number = 2): string {
+export function formatNumberToFI(num: number | undefined, minDigits = 2, maxDigits = 2): string {
     return num?.toLocaleString('fi-FI', { minimumFractionDigits: minDigits, maximumFractionDigits: maxDigits }) ?? '';
 }
 
 // Helper function to format number to Finnish locale with 2 decimal places and convert to Euros
-export function formatNumberToEuros(num: number | undefined, minDigits: number = 2, maxDigits: number = 2): string {
+export function formatNumberToEuros(num: number | undefined, minDigits = 2, maxDigits = 2): string {
     return ((num ?? 0) / 100).toLocaleString('fi-FI', { minimumFractionDigits: minDigits, maximumFractionDigits: maxDigits });
 }

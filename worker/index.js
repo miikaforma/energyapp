209387eff9e52189e526fddecfa945f0a787bd4f
miikaforma <miika.forma@codecontrol.fi>
2024-06-09 16:46:52 +0300

    Push notification changes

--- worker/index.js
+++ worker/index.js
@@ -7,6 +7,7 @@ self.addEventListener("push", (event) => {
       self.registration.showNotification(titleBuilder(data), {
         body: bodyBuilder(data),
         icon: "/android-chrome-192x192.png",
+        timestamp: data.updatedAt,
       }),
     );
   }
@@ -23,6 +24,33 @@ self.addEventListener("push", (event) => {
       }),
     );
   }
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        if (clientList.length > 0) {
+          let client = clientList[0];
+          for (let clientItem of clientList) {
+            if (clientItem.focused) {
+              client = clientItem;
+            }
+          }
+          return client.focus();
+        }
+        return clients.openWindow("/");
+      }),
+  );
+});
+
+self.addEventListener('notificationclose', event => {
+  const closedNotification = event.notification;
+  const notificationData = closedNotification.data;
+
+  console.log('Notification was closed:', notificationData);
 });
 
 function titleBuilder(data) {
@@ -50,15 +78,15 @@ function bodyBuilder(data) {
           minimumFractionDigits: 2,
           maximumFractionDigits: 2,
         })} c/kWh`,
-        `Yön hinta: ${Number(data.offPeak1).toLocaleString("fi-FI", {
+        `Yön hinta (01:00 - 09:00): ${Number(data.offPeak1).toLocaleString("fi-FI", {
           minimumFractionDigits: 2,
           maximumFractionDigits: 2,
         })} c/kWh`,
-        `Päivän hinta: ${Number(data.peak).toLocaleString("fi-FI", {
+        `Päivän hinta (09:00 - 21:00): ${Number(data.peak).toLocaleString("fi-FI", {
           minimumFractionDigits: 2,
           maximumFractionDigits: 2,
         })} c/kWh`,
-        `Illan hinta: ${Number(data.offPeak2).toLocaleString("fi-FI", {
+        `Illan hinta (21:00 - 01:00): ${Number(data.offPeak2).toLocaleString("fi-FI", {
           minimumFractionDigits: 2,
           maximumFractionDigits: 2,
         })} c/kWh`,
@@ -67,23 +95,3 @@ function bodyBuilder(data) {
     }
   }
 }
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  event.waitUntil(
-    self.clients
-      .matchAll({ type: "window", includeUncontrolled: true })
-      .then((clientList) => {
-        if (clientList.length > 0) {
-          let client = clientList[0];
-          for (let clientItem of clientList) {
-            if (clientItem.focused) {
-              client = clientItem;
-            }
-          }
-          return client.focus();
-        }
-        return clients.openWindow("/");
-      }),
-  );
-});
